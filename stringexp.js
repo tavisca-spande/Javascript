5284a6dc1390d7269bc588c25af1e42a2f3f320b
tavisca-spande <spande@tavisca.com>
2014-07-27 18:43:36 +0530

    this fixes #1 and also resolves #2 issues

--- stringexp.js
+++ stringexp.js
@@ -1,51 +1,3 @@
-
-
-	var stringoperations = function () {
-	var optionarr=document.getElementById("options");
-	var strtind = document.getElementById("startindex");
-	var endind = document.getElementById("endindex");
-	var str1 = document.getElementById("ipstring1");
-	var str2 = document.getElementById("ipstring2");
-	var str3= document.getElementById("ipstring3");
-	var res = document.getElementById("opstring");
-	//document.getElementById("options").addEventListener("onchange",stringoperations);
-	var s1 = new string_exp(str1.value);
-	var s2 = new string_exp(str2.value);
-		var optval = optionarr.options[optionarr.selectedIndex].text;
-	switch(optval)
-		{
-			case "Length":		
-				res.value = s1.strlength;
-			break;
-			case "Concate":
-				res.value = s1.concatenation(s2);
-			break;
-			case "SubString":
-				res.value = s1.mysubstring(strtind.value,endind.value);
-			 break;
-			 case "CharAt":
-			 res.value = s1.charat(strtind.value);
-			 break;
-
-			 case "indexof":
-			 res.value = s1.indexof(str2.value);
-			 break;
-
-			 case "lastindexof":
-			 res.value = s1.lastindexof(str2.value);
-			 break;
-
-			 case "Replace":
-			 res.value = s1.myreplace(str2.value,str3.value);
-			 break;
-		}
-
-
-}
-
-
-
-
 function string_exp(value){
 	this.value=value;
 	this.strlength=strlength.call(this);
@@ -159,6 +111,48 @@ var strlength = function()
     	   i++;
     return i;
 }   
+
+	var stringoperations = function () {
+	var optionarr=document.getElementById("options");
+	var strtind = document.getElementById("startindex");
+	var endind = document.getElementById("endindex");
+	var str1 = document.getElementById("ipstring1");
+	var str2 = document.getElementById("ipstring2");
+	var str3= document.getElementById("ipstring3");
+	var res = document.getElementById("opstring");
+	var s1 = new string_exp(str1.value);
+	var s2 = new string_exp(str2.value);
+		var optval = optionarr.options[optionarr.selectedIndex].text;
+	switch(optval)
+		{
+			case "Length":		
+				res.value = s1.strlength;
+			break;
+			case "Concate":
+				res.value = s1.concatenation(s2);
+			break;
+			case "SubString":
+				res.value = s1.mysubstring(strtind.value,endind.value);
+			 break;
+			 case "CharAt":
+			 res.value = s1.charat(strtind.value);
+			 break;
+
+			 case "indexof":
+			 res.value = s1.indexof(str2.value);
+			 break;
+
+			 case "lastindexof":
+			 res.value = s1.lastindexof(str2.value);
+			 break;
+
+			 case "Replace":
+			 res.value = s1.myreplace(str2.value,str3.value);
+			 break;
+		}
+}
+
+
 
 
 
